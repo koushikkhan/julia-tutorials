174a5aa7e53e47ea05c734634d91f9e824bb13c2
Koushik <koushik.khan@ihsmarkit.com>
2023-02-27 18:20:41 +0530

    scripts added

---
+++ scripts/01_julia_basics.jl
@@ -0,0 +1,13 @@
+### A Pluto.jl notebook ###
+# v0.19.22
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 5cbc4d44-cac7-46fc-9dc2-f66990b71212
+md"""
+# Test
+"""
+
+# ╔═╡ Cell order:
+# ╠═5cbc4d44-cac7-46fc-9dc2-f66990b71212
---
+++ scripts/02_stats_with_julia.jl
@@ -0,0 +1,38 @@
+### A Pluto.jl notebook ###
+# v0.19.22
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a763bd20-b694-11ed-083b-39056618dddd
+md"""
+# Statistics with Julia
+"""
+
+# ╔═╡ d0bf3399-d0c0-400a-8ac1-432b7223ccdf
+println("Hello Julia")
+
+# ╔═╡ b4e6298f-2234-49ce-b792-ef27243b0de5
+md"""
+# Define functions
+"""
+
+# ╔═╡ e0ace5dd-1826-4656-a53d-aaf39cc4b7b7
+begin
+function f(x, y)
+	c = x+y
+	return c
+end
+
+f(2,3)
+end
+
+# ╔═╡ c71912a9-c672-4eaf-b4e7-147a1aab88a1
+
+
+# ╔═╡ Cell order:
+# ╟─a763bd20-b694-11ed-083b-39056618dddd
+# ╠═d0bf3399-d0c0-400a-8ac1-432b7223ccdf
+# ╟─b4e6298f-2234-49ce-b792-ef27243b0de5
+# ╠═e0ace5dd-1826-4656-a53d-aaf39cc4b7b7
+# ╠═c71912a9-c672-4eaf-b4e7-147a1aab88a1
