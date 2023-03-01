a1e485d35cef173b61ea0b8629e3450a25fba57c
Koushik <koushik.khan@ihsmarkit.com>
2023-03-01 19:47:16 +0530

    scripts converted to notebooks

--- scripts/01_julia_basics.jl
+++ scripts/01_julia_basics.jl
@@ -1,13 +1,3 @@
-### A Pluto.jl notebook ###
-# v0.19.22
-
-using Markdown
-using InteractiveUtils
-
-# ╔═╡ 5cbc4d44-cac7-46fc-9dc2-f66990b71212
-md"""
-# Test
-"""
-
-# ╔═╡ Cell order:
-# ╠═5cbc4d44-cac7-46fc-9dc2-f66990b71212
+function test(x, y)
+    return (x + y)
+end
